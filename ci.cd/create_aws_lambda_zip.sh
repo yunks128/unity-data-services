56d549dcc43b9e68aafcf6057fb7d085041a3cd1
Wai Phyo <wai.phyo@jpl.nasa.gov>
2022-05-27 16:22:34 -0700

    feat: update ci/cd to include terraform

--- ci.cd/create_aws_lambda_zip.sh
+++ ci.cd/create_aws_lambda_zip.sh
@@ -3,10 +3,10 @@
 apt-get update -y && apt-get install zip -y
 
 ZIP_NAME='cumulus_lambda_functions_deployment.zip'
+TERRAFORM_ZIP_NAME='terraform_cumulus_lambda_functions_deployment.zip'
 project_root_dir=${GITHUB_WORKSPACE}
 zip_file="${project_root_dir}/$ZIP_NAME" ; # save the result file in current working directory
-
-tmp_proj='/tmp/cumulus_lambda_functions'
+terraform_zip_file="${project_root_dir}/$TERRAFORM_ZIP_NAME" ; # save the result file in current working directory
 
 source_dir=`python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])'`
 
@@ -14,6 +14,13 @@ cd ${source_dir}
 rm -rf ${zip_file} && \
 zip -r9 ${zip_file} . && \
 echo "zipped to ${zip_file}"
+
+cd ${project_root_dir}/tf-module/unity-cumulus
+mkdir build
+cp ${zip_file} build/
+
+cd $project_root_dir/tf-module
+zip -r9 ${terraform_zip_file} unity-cumulus
 
 software_version=`python3 ${project_root_dir}/setup.py --version`
 echo "software_version=${software_version}" >> ${GITHUB_ENV}
