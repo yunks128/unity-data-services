5f5362e77e440cf638229030b6cfe60ac4257a2e
Wai Phyo <wai.phyo@jpl.nasa.gov>
2022-05-27 16:59:25 -0700

    fix: add terraform zip as asset

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extra_requires = ['botocore', 'boto3',]
 
 setup(
     name="cumulus_lambda_functions",
-    version="1.2.0",
+    version="1.2.1",
     packages=find_packages(),
     install_requires=install_requires,
     tests_require=['mock', 'nose', 'sphinx', 'sphinx_rtd_theme', 'coverage'],
