a442ab25c11ebd0f5f47d97569051e9042b21f16
Wai Phyo <wai.phyo@jpl.nasa.gov>
2022-06-08 17:58:20 -0700

    chore: bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extra_requires = ['botocore', 'boto3',]
 
 setup(
     name="cumulus_lambda_functions",
-    version="1.3.3",
+    version="1.3.4",
     packages=find_packages(),
     install_requires=install_requires,
     tests_require=['mock', 'nose', 'sphinx', 'sphinx_rtd_theme', 'coverage'],
