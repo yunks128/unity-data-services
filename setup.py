7465f4411ae1fdeb378e4786cc2285cc7a45c37a
wphyojpl <38299756+wphyojpl@users.noreply.github.com>
2022-09-06 14:22:13 -0700

    fix: GitHub.action.update (#76)
    * fix: disable building upon pull request
    
    * chore: bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extra_requires = ['botocore', 'boto3',]
 
 setup(
     name="cumulus_lambda_functions",
-    version="1.6.18",
+    version="1.6.19",
     packages=find_packages(),
     install_requires=install_requires,
     tests_require=['mock', 'nose', 'sphinx', 'sphinx_rtd_theme', 'coverage'],
