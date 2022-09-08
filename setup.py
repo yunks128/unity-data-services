61cd1cc7f824e8cb4df93a180ed00fd726bf589a
wphyojpl <38299756+wphyojpl@users.noreply.github.com>
2022-09-08 16:25:49 -0700

    fix: not honoring offset and limit in Collection query (#82)
    * fix: not assigning limit and offset from api endpoint
    
    * chore: bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extra_requires = ['botocore', 'boto3',]
 
 setup(
     name="cumulus_lambda_functions",
-    version="1.7.1",
+    version="1.7.2",
     packages=find_packages(),
     install_requires=install_requires,
     tests_require=['mock', 'nose', 'sphinx', 'sphinx_rtd_theme', 'coverage', 'pystac', 'python-dotenv', 'jsonschema'],
