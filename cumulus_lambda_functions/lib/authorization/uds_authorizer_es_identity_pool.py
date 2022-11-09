190ba634668b11f8b27cb0dc839022914042e2fa
Wai Phyo <wai.phyo@jpl.nasa.gov>
2022-11-09 14:34:06 -0800

    feat: update auth index according to slides

--- cumulus_lambda_functions/lib/authorization/uds_authorizer_es_identity_pool.py
+++ cumulus_lambda_functions/lib/authorization/uds_authorizer_es_identity_pool.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import re
 
 from cumulus_lambda_functions.lib.authorization.uds_authorizer_abstract import UDSAuthorizorAbstract
 from cumulus_lambda_functions.lib.aws.aws_cognito import AwsCognito
@@ -12,14 +13,11 @@ LOGGER = logging.getLogger(__name__)
 
 class UDSAuthorizorEsIdentityPool(UDSAuthorizorAbstract):
 
-    def __init__(self, user_pool_id: str) -> None:
+    def __init__(self, user_pool_id: str, es_url: str, es_port=443) -> None:
         super().__init__()
-        es_url = os.getenv('ES_URL')  # TODO validation
-        self.__authorization_index = os.getenv('AUTHORIZATION_INDEX')  # LDAP_Group_Permission
-        es_port = int(os.getenv('ES_PORT', '443'))
         self.__cognito = AwsCognito(user_pool_id)
         self.__es: ESAbstract = ESFactory().get_instance('AWS',
-                                                         index=self.__authorization_index,
+                                                         index=DBConstants.authorization_index,
                                                          base_url=es_url,
                                                          port=es_port)
 
@@ -30,7 +28,7 @@ class UDSAuthorizorEsIdentityPool(UDSAuthorizorAbstract):
             DBConstants.tenant: tenant,
             DBConstants.tenant_venue: venue,
             DBConstants.authorized_group_name_key: ldap_group_name,
-        }, f'{tenant}__{venue}__{ldap_group_name}', self.__authorization_index)
+        }, f'{tenant}__{venue}__{ldap_group_name}', DBConstants.authorization_index)
         return
 
     def delete_authorized_group(self, tenant: str, venue: str, ldap_group_name: str):
@@ -73,7 +71,7 @@ class UDSAuthorizorEsIdentityPool(UDSAuthorizorAbstract):
             DBConstants.tenant: tenant,
             DBConstants.tenant_venue: venue,
             DBConstants.authorized_group_name_key: ldap_group_name,
-        }, f'{tenant}__{venue}__{ldap_group_name}', self.__authorization_index)
+        }, f'{tenant}__{venue}__{ldap_group_name}', DBConstants.authorization_index)
         return
 
     def get_authorized_tenant(self, username: str, action: str, resource: str) -> list:
@@ -104,18 +102,52 @@ class UDSAuthorizorEsIdentityPool(UDSAuthorizorAbstract):
         })
         return [k['_source'] for k in authorized_groups['hits']['hits']]
 
-    def authorize(self, username, resource, action) -> bool:
+    def is_authorized_for_collection(self, action: str, collection_id: str, username: str, tenant: str, venue: str):
         belonged_groups = set(self.__cognito.get_groups(username))
-        authorized_groups = self.__es.query({
+        authorization_dsl = {
+            '_source': [DBConstants.resource_key],
+            'size': 9999,
             'query': {
-                'match_all': {}  # TODO
+                'bool': {
+                    'must': [
+                        {'terms': {DBConstants.authorized_group_name_key: list(belonged_groups)}},
+                        {'term': {DBConstants.action_key: action}},
+                        {'term': {DBConstants.tenant: tenant}},
+                        {'term': {DBConstants.tenant_venue: venue}},
+                    ]
+                }
             }
-        })
-        LOGGER.debug(f'belonged_groups for {username}: {belonged_groups}')
-        authorized_groups = set([k['_source']['group_name'] for k in authorized_groups['hits']['hits']])
-        LOGGER.debug(f'authorized_groups for {resource}-{action}: {authorized_groups}')
-        if any([k in authorized_groups for k in belonged_groups]):
-            LOGGER.debug(f'{username} is authorized for {resource}-{action}')
-            return True
-        LOGGER.debug(f'{username} is NOT authorized for {resource}-{action}')
+        }
+        authorized_collection_map = self.__es.query(authorization_dsl)
+        authorized_collection_map = [k['_source'][DBConstants.resource_key] for k in authorized_collection_map['hits']['hits']]
+        authorized_collection_map = [item for sublist in authorized_collection_map for item in sublist]
+        for each_collection_regex in authorized_collection_map:
+            LOGGER.debug(f'comparing regex: {each_collection_regex} vs {collection_id}')
+            version_regex = re.compile(each_collection_regex)
+            if version_regex.match(collection_id):
+                return True
         return False
+
+    def get_authorized_collections(self, action: str, username: str, tenant: str = '', venue: str = ''):
+        belonged_groups = set(self.__cognito.get_groups(username))
+        authorization_dsl = {
+            '_source': [DBConstants.resource_key],
+            'size': 9999,
+            'query': {
+                'bool': {
+                    'must': [
+                        {'terms': {DBConstants.authorized_group_name_key: list(belonged_groups)}},
+                        {'term': {DBConstants.action_key: action}},
+                    ]
+                }
+            }
+        }
+        if tenant != '':
+            authorization_dsl['query']['bool']['must'].append({'term': {DBConstants.tenant: tenant}})
+        if venue != '':
+            authorization_dsl['query']['bool']['must'].append({'term': {DBConstants.tenant_venue: venue}})
+        authorized_collection_map = self.__es.query(authorization_dsl)
+        authorized_collection_map = [k['_source'][DBConstants.resource_key] for k in authorized_collection_map['hits']['hits']]
+        authorized_collection_map = [item for sublist in authorized_collection_map for item in sublist]
+        return authorized_collection_map
+
