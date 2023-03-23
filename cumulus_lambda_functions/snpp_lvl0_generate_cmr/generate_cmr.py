224a941e8e2589b02a4512a4d905b62d3554ad2b
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-03-23 11:27:23 -0700

    feat: configurable file postfixes for PDS metadata extraction

--- cumulus_lambda_functions/snpp_lvl0_generate_cmr/generate_cmr.py
+++ cumulus_lambda_functions/snpp_lvl0_generate_cmr/generate_cmr.py
@@ -97,6 +97,8 @@ class GenerateCmr:
         self.__event = event
         self.__s3 = AwsS3()
         self._pds_file_dict = None
+        self.__file_postfixes = os.getenv('FILE_POSTFIX', '1.PDS.XML, NC.XML')
+        self.__file_postfixes = [k.upper().strip() for k in self.__file_postfixes.split(',')]
         self.__input_file_list = []
 
     def __validate_input(self):
@@ -109,7 +111,8 @@ class GenerateCmr:
         self.__input_file_list = self.__event['cma']['event']['meta']['input_granules'][0]['files']
         for each_file in self.__input_file_list:
             LOGGER.debug(f'checking file: {each_file}')
-            if each_file['key'].upper().endswith('1.PDS.XML'):
+            file_key_upper = each_file['key'].upper().strip()
+            if any([file_key_upper.endswith(k) for k in self.__file_postfixes]):
                 return each_file
         return None
 
