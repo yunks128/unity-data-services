2262b4c9ae710a983171322d2e9aa659acc31d44
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-06-13 06:21:54 -0700

    feat: refactor download classes to use inheritance

--- cumulus_lambda_functions/stage_in_out/download_granules_abstract.py
+++ cumulus_lambda_functions/stage_in_out/download_granules_abstract.py
@@ -3,7 +3,7 @@ import logging
 import os
 from abc import ABC, abstractmethod
 
-from pystac import ItemCollection
+from pystac import ItemCollection, Asset, Item
 
 from cumulus_lambda_functions.lib.utils.file_utils import FileUtils
 
@@ -20,6 +20,27 @@ class DownloadGranulesAbstract(ABC):
         self._granules_json: ItemCollection = {}
         self._download_dir = '/tmp'
         self._downloading_keys = set([k.strip() for k in os.environ.get(self.DOWNLOADING_KEYS, 'data').strip().split(',')])
+
+    def _set_props_from_env(self):
+        raise NotImplementedError(f'to be implemented in concrete classes')
+
+    def _download_one_item(self, downloading_url):
+        raise NotImplementedError(f'to be implemented in concrete classes')
+
+    def _download_one_granule_item(self, granule_item: Item):
+        new_asset_dict = {}
+        for name, value_dict in granule_item.assets.items():
+            if name not in self._downloading_keys:
+                LOGGER.debug(f'skipping {name}. Not in downloading keys')
+                continue
+            value_dict: Asset = value_dict
+            downloading_url = value_dict.href
+            LOGGER.debug(f'downloading: {downloading_url}')
+            self._download_one_item(downloading_url)
+            value_dict.href = os.path.join('.', os.path.basename(downloading_url))
+            new_asset_dict[name] = value_dict
+        granule_item.assets = new_asset_dict
+        return granule_item
 
     def _setup_download_dir(self):
         self._download_dir = os.environ.get(self.DOWNLOAD_DIR_KEY)
@@ -43,6 +64,28 @@ class DownloadGranulesAbstract(ABC):
         self._granules_json = ItemCollection.from_dict(json_stac)
         return self
     
-    @abstractmethod
     def download(self, **kwargs) -> dict:
-        raise NotImplementedError()
+        self._set_props_from_env()
+        LOGGER.debug(f'creating download dir: {self._download_dir}')
+        if len(self._granules_json.items) < 1:
+            LOGGER.warning(f'cannot find any granules')
+            return self._granules_json.to_dict(False)
+        local_items = []
+        error_list = []
+        for each_item in self._granules_json.items:
+            try:
+                local_item = self._download_one_granule_item(each_item)
+                local_items.append(local_item)
+            except Exception as e:
+                LOGGER.exception(f'error downloading granule: {each_item.id}')
+                error_list.append({'error': str(e), 'id': each_item.id, })
+        LOGGER.debug(f'finished downloading all granules')
+        self._granules_json.items = local_items
+        LOGGER.debug(f'writing features collection json to downloading directory')
+        granules_json_dict = self._granules_json.to_dict(False)
+        FileUtils.write_json(os.path.join(self._download_dir, 'downloaded_feature_collection.json'), granules_json_dict, overwrite=True, prettify=True)
+        LOGGER.debug(f'writing errors if any')
+        if len(error_list) > 0:
+            with open(f'{self._download_dir}/error.log', 'w') as error_file:
+                error_file.write(json.dumps(error_list, indent=4))
+        return granules_json_dict
