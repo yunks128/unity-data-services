6c26b0b948dd127c6ba0821c1ea20deda65658bf
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-06-13 06:21:54 -0700

    feat: refactor download classes to use inheritance

--- cumulus_lambda_functions/stage_in_out/download_granules_daac.py
+++ cumulus_lambda_functions/stage_in_out/download_granules_daac.py
@@ -1,8 +1,6 @@
 import requests
-from pystac import ItemCollection, Item, Asset
 
 from cumulus_lambda_functions.lib.earthdata_login.urs_token_retriever import URSTokenRetriever
-from cumulus_lambda_functions.lib.utils.file_utils import FileUtils
 from cumulus_lambda_functions.stage_in_out.download_granules_abstract import DownloadGranulesAbstract
 import json
 import logging
@@ -18,7 +16,7 @@ class DownloadGranulesDAAC(DownloadGranulesAbstract):
         super().__init__()
         self.__edl_token = None
 
-    def __set_props_from_env(self):
+    def _set_props_from_env(self):
         missing_keys = [k for k in [self.STAC_JSON, self.DOWNLOAD_DIR_KEY] if k not in os.environ]
         if len(missing_keys) > 0:
             raise ValueError(f'missing environment keys: {missing_keys}')
@@ -27,50 +25,13 @@ class DownloadGranulesDAAC(DownloadGranulesAbstract):
         self.__edl_token = URSTokenRetriever().start()
         return self
 
-    def __download_one_granule_item(self, granule_item: Item):
+    def _download_one_item(self, downloading_url):
         headers = {
             'Authorization': f'Bearer {self.__edl_token}'
         }
-        new_asset_dict = {}
-        for name, value_dict in granule_item.assets.items():
-            if name not in self._downloading_keys:
-                LOGGER.debug(f'skipping {name}. Not in downloading keys')
-                continue
-            value_dict: Asset = value_dict
-            downloading_url = value_dict.href
-            LOGGER.debug(f'downloading: {downloading_url}')
-            r = requests.get(downloading_url, headers=headers)
-            r.raise_for_status()
-            local_file_path = os.path.join(self._download_dir, os.path.basename(downloading_url))
-            with open(local_file_path, 'wb') as fd:
-                fd.write(r.content)
-            value_dict.href = os.path.join('.', os.path.basename(downloading_url))
-            new_asset_dict[name] = value_dict
-        granule_item.assets = new_asset_dict
-        return granule_item
-
-    def download(self, **kwargs) -> dict:
-        self.__set_props_from_env()
-        LOGGER.debug(f'creating download dir: {self._download_dir}')
-        if len(self._granules_json.items) < 1:
-            LOGGER.warning(f'cannot find any granules')
-            return self._granules_json.to_dict(False)
-        local_items = []
-        error_list = []
-        for each_item in self._granules_json.items:
-            try:
-                local_item = self.__download_one_granule_item(each_item)
-                local_items.append(local_item)
-            except Exception as e:
-                LOGGER.exception(f'error downloading granule: {each_item.id}')
-                error_list.append({'error': str(e), 'id': each_item.id, })
-        LOGGER.debug(f'finished downloading all granules')
-        self._granules_json.items = local_items
-        LOGGER.debug(f'writing features collection json to downloading directory')
-        granules_json_dict = self._granules_json.to_dict(False)
-        FileUtils.write_json(os.path.join(self._download_dir, 'downloaded_feature_collection.json'), granules_json_dict, overwrite=True, prettify=True)
-        LOGGER.debug(f'writing errors if any')
-        if len(error_list) > 0:
-            with open(f'{self._download_dir}/error.log', 'w') as error_file:
-                error_file.write(json.dumps(error_list, indent=4))
-        return granules_json_dict
+        r = requests.get(downloading_url, headers=headers)
+        r.raise_for_status()
+        local_file_path = os.path.join(self._download_dir, os.path.basename(downloading_url))
+        with open(local_file_path, 'wb') as fd:
+            fd.write(r.content)
+        return local_file_path
--- cumulus_lambda_functions/stage_in_out/download_granules_http.py
+++ cumulus_lambda_functions/stage_in_out/download_granules_http.py
@@ -15,7 +15,7 @@ class DownloadGranulesHttp(DownloadGranulesAbstract):
     def __init__(self) -> None:
         super().__init__()
 
-    def __set_props_from_env(self):
+    def _set_props_from_env(self):
         missing_keys = [k for k in [self.STAC_JSON, self.DOWNLOAD_DIR_KEY] if k not in os.environ]
         if len(missing_keys) > 0:
             raise ValueError(f'missing environment keys: {missing_keys}')
@@ -23,71 +23,11 @@ class DownloadGranulesHttp(DownloadGranulesAbstract):
         self._setup_download_dir()
         return self
 
-    def __get_downloading_urls(self, granules_result: list):
-        if len(granules_result) < 1:
-            LOGGER.warning(f'cannot find any granules')
-            return []
-        downloading_urls = [k['assets'] for k in granules_result]
-        return downloading_urls
-
-    def __download_one_granule(self, assets: dict):
-        """
-        sample assets
-          {
-            "data": {
-              "href": "s3://am-uds-dev-cumulus-internal/ATMS_SCIENCE_Group___1/P1570515ATMSSCIENCEAAT16017044853900.PDS",
-              "title": "P1570515ATMSSCIENCEAAT16017044853900.PDS",
-              "description": "P1570515ATMSSCIENCEAAT16017044853900.PDS"
-            },
-            "metadata__data": {
-              "href": "s3://am-uds-dev-cumulus-internal/ATMS_SCIENCE_Group___1/P1570515ATMSSCIENCEAAT16017044853901.PDS",
-              "title": "P1570515ATMSSCIENCEAAT16017044853901.PDS",
-              "description": "P1570515ATMSSCIENCEAAT16017044853901.PDS"
-            },
-            "metadata__xml": {
-              "href": "s3://am-uds-dev-cumulus-internal/ATMS_SCIENCE_Group___1/P1570515ATMSSCIENCEAAT16017044853901.PDS.xml",
-              "title": "P1570515ATMSSCIENCEAAT16017044853901.PDS.xml",
-              "description": "P1570515ATMSSCIENCEAAT16017044853901.PDS.xml"
-            },
-            "metadata__cmr": {
-              "href": "s3://am-uds-dev-cumulus-internal/ATMS_SCIENCE_Group___1/P1570515ATMSSCIENCEAAT16017044853900.PDS.cmr.xml",
-              "title": "P1570515ATMSSCIENCEAAT16017044853900.PDS.cmr.xml",
-              "description": "P1570515ATMSSCIENCEAAT16017044853900.PDS.cmr.xml"
-            }
-          }
-        :param assets:
-        :return:
-        """
-        error_log = []
-        local_item = {}
-        for k, v in assets.items():
-            local_item[k] = v
-            try:
-                LOGGER.debug(f'downloading: {v["href"]}')
-                downloading_response = requests.get(v['href'])
-                downloading_response.raise_for_status()
-                downloading_response.raw.decode_content = True
-                local_file_path = os.path.join(self._download_dir, os.path.basename(v["href"]))
-                with open(local_file_path, 'wb') as f:
-                    shutil.copyfileobj(downloading_response.raw, f)
-                local_item[k]['href'] = local_file_path
-            except Exception as e:
-                LOGGER.exception(f'failed to download {v}')
-                local_item[k]['description'] = f'download failed. {str(e)}'
-                error_log.append(v)
-        return local_item, error_log
-
-    def download(self, **kwargs) -> list:
-        self.__set_props_from_env()
-        downloading_urls = self.__get_downloading_urls(self._granules_json)
-        error_list = []
-        local_items = []
-        for each in downloading_urls:
-            LOGGER.debug(f'working on {each}')
-            local_item, current_error_list = self.__download_one_granule(each)
-            local_items.append({'assets': local_item})
-            error_list.extend(current_error_list)
-        if len(error_list) > 0:
-            with open(f'{self._download_dir}/error.log', 'w') as error_file:
-                error_file.write(json.dumps(error_list, indent=4))
-        return local_items
+    def _download_one_item(self, downloading_url):
+        downloading_response = requests.get(v['href'])
+        downloading_response.raise_for_status()
+        downloading_response.raw.decode_content = True
+        local_file_path = os.path.join(self._download_dir, os.path.basename(v["href"]))
+        with open(local_file_path, 'wb') as f:
+            shutil.copyfileobj(downloading_response.raw, f)
+        return local_file_path
