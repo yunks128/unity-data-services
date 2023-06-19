208f2f024a8cdd30f6cffe62b21e9760342abb4d
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-06-19 16:37:00 -0700

    feat: new upload handler for relative paths + user granule-id from the granule-stac-json

--- cumulus_lambda_functions/stage_in_out/upoad_granules_factory.py
+++ cumulus_lambda_functions/stage_in_out/upoad_granules_factory.py
@@ -1,6 +1,7 @@
 class UploadGranulesFactory:
     S3 = 'S3'
     CATALOG_S3 = 'CATALOG_S3'
+    CATALOG_NO_DAPA_S3 = 'CATALOG_NO_DAPA_S3'
 
     def get_class(self, upload_type):
         if upload_type == UploadGranulesFactory.S3:
@@ -9,4 +10,7 @@ class UploadGranulesFactory:
         if upload_type == UploadGranulesFactory.CATALOG_S3:
             from cumulus_lambda_functions.stage_in_out.upload_granules_by_catalog_s3 import UploadGranulesByCatalogS3
             return UploadGranulesByCatalogS3()
+        if upload_type == UploadGranulesFactory.CATALOG_NO_DAPA_S3:
+            from cumulus_lambda_functions.stage_in_out.upload_granules_by_complete_catalog_s3 import UploadGranulesByCompleteCatalogS3
+            return UploadGranulesByCompleteCatalogS3()
         raise ValueError(f'unknown search_type: {upload_type}')
