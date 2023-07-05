5883643548b490f9a396f3858a19d34edb304823
wphyojpl <38299756+wphyojpl@users.noreply.github.com>
2023-07-05 14:04:03 -0700

    feat: parallelize download (#169)
    * feat: starting parallel job manager
    
    * fix: get parallel process working for download
    
    * feat: update testcases
    
    * chore: update version
    
    * chore: update readme
    
    * feat: add configurable parallel count

---
+++ cumulus_lambda_functions/lib/processing_jobs/job_manager_memory.py
@@ -0,0 +1,33 @@
+from cumulus_lambda_functions.lib.processing_jobs.job_manager_abstract import JobManagerAbstract, JobManagerProps
+
+
+class JobManagerMemory(JobManagerAbstract):
+    def __init__(self, props=JobManagerProps()) -> None:
+        self.__props = props
+        self.__processing_job_dict = {}
+
+    def get_all_job_files(self):
+        return [k for k in self.__props.memory_job_dict.keys()]
+
+    def get_job_file(self, job_path, validate_job_content=lambda x: True):
+        if job_path not in self.__props.memory_job_dict:
+            return None
+        job = self.__props.memory_job_dict.get(job_path)
+        del self.__props.memory_job_dict[job_path]
+        if not validate_job_content(job):
+            return None
+        self.__processing_job_dict[job_path] = job
+        return job
+
+    def remove_from_processing(self, job_path):
+        if job_path not in self.__processing_job_dict:
+            return
+        del self.__processing_job_dict[job_path]
+        pass
+
+    def put_back_failed_job(self, original_job_path: str):
+        if original_job_path not in self.__processing_job_dict:
+            return
+        self.__props.memory_job_dict[original_job_path] = self.__processing_job_dict[original_job_path]
+        del self.__processing_job_dict[original_job_path]
+        return
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ extra_requires = ['botocore', 'boto3',]
 
 setup(
     name="cumulus_lambda_functions",
-    version="5.1.0",
+    version="5.2.0",
     packages=find_packages(),
     install_requires=install_requires,
     tests_require=['mock', 'nose', 'sphinx', 'sphinx_rtd_theme', 'coverage', 'pystac', 'python-dotenv', 'jsonschema'],
