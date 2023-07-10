6e03fe45b7e7c71f5c06aa0dac04e6e4219c9894
wphyojpl <38299756+wphyojpl@users.noreply.github.com>
2023-07-10 15:00:37 -0700

    fix: download error (#182)
    * fix: retry 5 times + wait 45 sec for DAAC
    
    * feat: make re-try configurable
    
    * fix: retry on Temporary failure in name resolution
    
    * chore: update version
    
    * fix: add new test case for error

--- tests/cumulus_lambda_functions/lib/earthdata_login/test_urs_token_retriever.py
+++ tests/cumulus_lambda_functions/lib/earthdata_login/test_urs_token_retriever.py
@@ -16,3 +16,14 @@ class TestURSTokenRetriever(TestCase):
         result = URSTokenRetriever().start()
         self.assertTrue(len(result) > 0, 'empty token')
         return
+
+    def test_02(self):
+        os.environ[Constants.EDL_USERNAME] = '/unity/uds/user/wphyo/edl_username'
+        os.environ[Constants.EDL_PASSWORD] = '/unity/uds/user/wphyo/edl_dwssap'
+        # os.environ[Constants.USERNAME] = 'usps_username'
+        # os.environ[Constants.PASSWORD] = 'usps_password'
+        os.environ[Constants.EDL_PASSWORD_TYPE] = Constants.PARAM_STORE
+        os.environ[Constants.EDL_BASE_URL] = 'urs1.earthdata.nasa.gov'
+        with self.assertRaises(RuntimeError) as cm:
+            result = URSTokenRetriever().start()
+        return
