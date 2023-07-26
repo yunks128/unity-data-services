f7823fc61c37ccadee5564b29b53658b2e1a0152
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-07-26 12:47:04 -0700

    fix: allow slash and no slash

--- cumulus_lambda_functions/uds_api/routes_api.py
+++ cumulus_lambda_functions/uds_api/routes_api.py
@@ -7,7 +7,7 @@ from cumulus_lambda_functions.uds_api import collections_api, granules_api
 # from ideas_api.src.endpoints import setup_es
 
 
-main_router = APIRouter()
+main_router = APIRouter(redirect_slashes=True)
 # main_router.include_router(setup_es.router)
 main_router.include_router(collections_api.router)
 main_router.include_router(granules_api.router)
