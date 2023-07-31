02ee0bced2954ca6a493950a1ec754ced76e2aa5
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-07-31 08:52:46 -0700

    fix: trying slash and no-slash

--- cumulus_lambda_functions/uds_api/web_service.py
+++ cumulus_lambda_functions/uds_api/web_service.py
@@ -1,16 +1,19 @@
-import logging
-
+# import logging
+from cumulus_lambda_functions.lib.lambda_logger_generator import LambdaLoggerGenerator
 from dotenv import load_dotenv
 from mangum.types import LambdaEvent, LambdaContext
 
 load_dotenv()
-LOGGER = logging.getLogger(__name__)
+LOGGER = LambdaLoggerGenerator.get_logger(__name__, LambdaLoggerGenerator.get_level_from_env())
+# LOGGER = logging.getLogger(__name__)
 
 import uvicorn
 from fastapi import FastAPI
 from mangum import Mangum
 from starlette.requests import Request
 
+from contextlib import ExitStack
+from mangum.protocols import HTTPCycle, LifespanCycle
 
 from cumulus_lambda_functions.uds_api.routes_api import main_router
 from cumulus_lambda_functions.uds_api.web_service_constants import WebServiceConstants
@@ -28,7 +31,21 @@ class MyMangum(Mangum):
 
     def __call__(self, event: LambdaEvent, context: LambdaContext) -> dict:
         try:
-            return super().__call__(event, context)
+            chosen_handler = self.infer(event, context)
+            LOGGER.info(f'{type(chosen_handler)}')
+            with ExitStack() as stack:
+                LOGGER.info(f'lifespan: {self.lifespan}')
+                if self.lifespan in ("auto", "on"):
+                    LOGGER.info(f'lifespan: {self.lifespan}')
+                    lifespan_cycle = LifespanCycle(self.app, self.lifespan)
+                    stack.enter_context(lifespan_cycle)
+                LOGGER.info(f'starting httpCycle: {chosen_handler.scope}.. {chosen_handler.body}')
+                http_cycle = HTTPCycle(chosen_handler.scope, chosen_handler.body)
+                LOGGER.info(f'calling it?')
+                http_response = http_cycle(self.app)
+                LOGGER.info(f'http_response {http_response}')
+
+                return chosen_handler(http_response)
         except Exception as e:
             LOGGER.exception(f'error in mangum.')
             raise e
