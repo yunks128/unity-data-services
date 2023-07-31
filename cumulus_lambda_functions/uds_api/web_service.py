e57d88c62e35b931ee1d7947081088633068fe7b
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-07-31 08:09:09 -0700

    fix: trying slash and no-slash

--- cumulus_lambda_functions/uds_api/web_service.py
+++ cumulus_lambda_functions/uds_api/web_service.py
@@ -1,5 +1,10 @@
+import logging
+
 from dotenv import load_dotenv
+from mangum.types import LambdaEvent, LambdaContext
+
 load_dotenv()
+LOGGER = logging.getLogger(__name__)
 
 import uvicorn
 from fastapi import FastAPI
@@ -19,8 +24,17 @@ async def root(request: Request):
     return {"message": "Hello World", "root_path": request.scope.get("root_path")}
 
 
+class MyMangum(Mangum):
+
+    def __call__(self, event: LambdaEvent, context: LambdaContext) -> dict:
+        try:
+            return super().__call__(event, context)
+        except Exception as e:
+            LOGGER.exception(f'error in mangum.')
+            raise e
+
 # to make it work with Amazon Lambda, we create a handler object
-handler = Mangum(app=app)
+handler = MyMangum(app=app)
 
 if __name__ == '__main__':
     uvicorn.run("web_service:app", port=8005, log_level="info", reload=True)
