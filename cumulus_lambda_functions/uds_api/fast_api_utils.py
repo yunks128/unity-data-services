d8b569cf7b47a8c0734fd5a37f9f2930ecc3808d
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-08-08 15:59:18 -0500

    fix: need token now for internal call

--- cumulus_lambda_functions/uds_api/fast_api_utils.py
+++ cumulus_lambda_functions/uds_api/fast_api_utils.py
@@ -1,7 +1,10 @@
 import base64
 import json
 
+from cumulus_lambda_functions.lib.lambda_logger_generator import LambdaLoggerGenerator
 from fastapi import APIRouter, HTTPException, Request, Response
+
+LOGGER = LambdaLoggerGenerator.get_logger(__name__, LambdaLoggerGenerator.get_level_from_env())
 
 
 class FastApiUtils:
@@ -13,8 +16,10 @@ class FastApiUtils:
         action = request.method
         resource = request.url.path
         bearer_token = request.headers.get('Authorization', '')
+        LOGGER.debug(f'raw bearer_token: {bearer_token}')
         username_part = bearer_token.split('.')[1]
         jwt_decoded = base64.standard_b64decode(f'{username_part}========'.encode()).decode()
+        LOGGER.debug(f'jwt_decoded: {jwt_decoded}')
         jwt_decoded = json.loads(jwt_decoded)
         ldap_groups = jwt_decoded['cognito:groups']
         username = jwt_decoded['username']
