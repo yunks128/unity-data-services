c8bb04769f79c6d4afbfbbcae784193da84b3334
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-08-28 15:36:55 -0700

    feat: use parallel upload

--- tests/integration_tests/test_docker_entry.py
+++ tests/integration_tests/test_docker_entry.py
@@ -1694,132 +1694,136 @@ class TestDockerEntry(TestCase):
             os.environ['CATALOG_FILE'] = os.path.join(tmp_dir_name, 'catalog.json')
             granules_dir = os.path.join(tmp_dir_name, 'some_granules')
             FileUtils.mk_dir_p(granules_dir)
-            with open(os.path.join(granules_dir, 'test_file01.nc'), 'w') as ff:
-                ff.write('sample_file')
-            with open(os.path.join(granules_dir, 'test_file01.nc.cas'), 'w') as ff:
-                ff.write('''<?xml version="1.0" encoding="UTF-8" ?>
-        <cas:metadata xmlns:cas="http://oodt.jpl.nasa.gov/1.0/cas">
-            <keyval type="scalar">
-                <key>AggregateDir</key>
-                <val>snppatmsl1a</val>
-            </keyval>
-            <keyval type="vector">
-                <key>AutomaticQualityFlag</key>
-                <val>Passed</val>
-            </keyval>
-            <keyval type="vector">
-                <key>BuildId</key>
-                <val>v01.43.00</val>
-            </keyval>
-            <keyval type="vector">
-                <key>CollectionLabel</key>
-                <val>L1AMw_nominal2</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>DataGroup</key>
-                <val>sndr</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>EndDateTime</key>
-                <val>2016-01-14T10:06:00.000Z</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>EndTAI93</key>
-                <val>726919569.000</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>FileFormat</key>
-                <val>nc4</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>FileLocation</key>
-                <val>/pge/out</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>Filename</key>
-                <val>SNDR.SNPP.ATMS.L1A.nominal2.02.nc</val>
-            </keyval>
-            <keyval type="vector">
-                <key>GranuleNumber</key>
-                <val>101</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>JobId</key>
-                <val>f163835c-9945-472f-bee2-2bc12673569f</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>ModelId</key>
-                <val>urn:npp:SnppAtmsL1a</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>NominalDate</key>
-                <val>2016-01-14</val>
-            </keyval>
-            <keyval type="vector">
-                <key>ProductName</key>
-                <val>SNDR.SNPP.ATMS.20160114T1000.m06.g101.L1A.L1AMw_nominal2.v03_15_00.D.201214135000.nc</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>ProductType</key>
-                <val>SNDR_SNPP_ATMS_L1A</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>ProductionDateTime</key>
-                <val>2020-12-14T13:50:00.000Z</val>
-            </keyval>
-            <keyval type="vector">
-                <key>ProductionLocation</key>
-                <val>Sounder SIPS: JPL/Caltech (Dev)</val>
-            </keyval>
-            <keyval type="vector">
-                <key>ProductionLocationCode</key>
-                <val>D</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>RequestId</key>
-                <val>1215</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>StartDateTime</key>
-                <val>2016-01-14T10:00:00.000Z</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>StartTAI93</key>
-                <val>726919209.000</val>
-            </keyval>
-            <keyval type="scalar">
-                <key>TaskId</key>
-                <val>8c3ae101-8f7c-46c8-b5c6-63e7b6d3c8cd</val>
-            </keyval>
-        </cas:metadata>''')
-            stac_item = Item(id='test_file01',
-                             geometry={
-                                "type": "Point",
-                                "coordinates": [0.0, 0.0]
-                             },
-                             bbox=[0.0, 0.0, 0.0, 0.0],
-                             datetime=TimeUtils().parse_from_unix(0, True).get_datetime_obj(),
-                             properties={
-                                 "start_datetime": "2016-01-31T18:00:00.009057Z",
-                                 "end_datetime": "2016-01-31T19:59:59.991043Z",
-                                 "created": "2016-02-01T02:45:59.639000Z",
-                                 "updated": "2022-03-23T15:48:21.578000Z",
-                                 "datetime": "2022-03-23T15:48:19.079000Z"
-                             },
-                             href=os.path.join('some_granules', 'test_file01.nc.stac.json'),
-                             collection='NA',
-                             assets={
-                                'data': Asset(os.path.join('.', 'test_file01.nc'), title='main data'),
-                                'metadata__cas': Asset(os.path.join('.', 'test_file01.nc.cas'), title='metadata cas'),
-                                'metadata__stac': Asset(os.path.join('.', 'test_file01.nc.stac.json'), title='metadata stac'),
-                             })
-            with open(os.path.join(granules_dir, 'test_file01.nc.stac.json'), 'w') as ff:
-                ff.write(json.dumps(stac_item.to_dict(False, False)))
             catalog = Catalog(
                 id='NA',
                 description='NA')
             catalog.set_self_href(os.environ['CATALOG_FILE'])
-            catalog.add_link(Link('item', os.path.join('some_granules', 'test_file01.nc.stac.json'), 'application/json'))
+
+            total_files = 10
+            for i in range(1, total_files+1):
+                filename = f'test_file{i:02d}'
+                with open(os.path.join(granules_dir, f'{filename}.nc'), 'w') as ff:
+                    ff.write('sample_file')
+                with open(os.path.join(granules_dir, f'{filename}.nc.cas'), 'w') as ff:
+                    ff.write('''<?xml version="1.0" encoding="UTF-8" ?>
+            <cas:metadata xmlns:cas="http://oodt.jpl.nasa.gov/1.0/cas">
+                <keyval type="scalar">
+                    <key>AggregateDir</key>
+                    <val>snppatmsl1a</val>
+                </keyval>
+                <keyval type="vector">
+                    <key>AutomaticQualityFlag</key>
+                    <val>Passed</val>
+                </keyval>
+                <keyval type="vector">
+                    <key>BuildId</key>
+                    <val>v01.43.00</val>
+                </keyval>
+                <keyval type="vector">
+                    <key>CollectionLabel</key>
+                    <val>L1AMw_nominal2</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>DataGroup</key>
+                    <val>sndr</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>EndDateTime</key>
+                    <val>2016-01-14T10:06:00.000Z</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>EndTAI93</key>
+                    <val>726919569.000</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>FileFormat</key>
+                    <val>nc4</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>FileLocation</key>
+                    <val>/pge/out</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>Filename</key>
+                    <val>SNDR.SNPP.ATMS.L1A.nominal2.02.nc</val>
+                </keyval>
+                <keyval type="vector">
+                    <key>GranuleNumber</key>
+                    <val>101</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>JobId</key>
+                    <val>f163835c-9945-472f-bee2-2bc12673569f</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>ModelId</key>
+                    <val>urn:npp:SnppAtmsL1a</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>NominalDate</key>
+                    <val>2016-01-14</val>
+                </keyval>
+                <keyval type="vector">
+                    <key>ProductName</key>
+                    <val>SNDR.SNPP.ATMS.20160114T1000.m06.g101.L1A.L1AMw_nominal2.v03_15_00.D.201214135000.nc</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>ProductType</key>
+                    <val>SNDR_SNPP_ATMS_L1A</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>ProductionDateTime</key>
+                    <val>2020-12-14T13:50:00.000Z</val>
+                </keyval>
+                <keyval type="vector">
+                    <key>ProductionLocation</key>
+                    <val>Sounder SIPS: JPL/Caltech (Dev)</val>
+                </keyval>
+                <keyval type="vector">
+                    <key>ProductionLocationCode</key>
+                    <val>D</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>RequestId</key>
+                    <val>1215</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>StartDateTime</key>
+                    <val>2016-01-14T10:00:00.000Z</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>StartTAI93</key>
+                    <val>726919209.000</val>
+                </keyval>
+                <keyval type="scalar">
+                    <key>TaskId</key>
+                    <val>8c3ae101-8f7c-46c8-b5c6-63e7b6d3c8cd</val>
+                </keyval>
+            </cas:metadata>''')
+                stac_item = Item(id=filename,
+                                 geometry={
+                                    "type": "Point",
+                                    "coordinates": [0.0, 0.0]
+                                 },
+                                 bbox=[0.0, 0.0, 0.0, 0.0],
+                                 datetime=TimeUtils().parse_from_unix(0, True).get_datetime_obj(),
+                                 properties={
+                                     "start_datetime": "2016-01-31T18:00:00.009057Z",
+                                     "end_datetime": "2016-01-31T19:59:59.991043Z",
+                                     "created": "2016-02-01T02:45:59.639000Z",
+                                     "updated": "2022-03-23T15:48:21.578000Z",
+                                     "datetime": "2022-03-23T15:48:19.079000Z"
+                                 },
+                                 href=os.path.join('some_granules', f'{filename}.nc.stac.json'),
+                                 collection='NA',
+                                 assets={
+                                    'data': Asset(os.path.join('.', 'test_file01.nc'), title='main data'),
+                                    'metadata__cas': Asset(os.path.join('.', 'test_file01.nc.cas'), title='metadata cas'),
+                                    'metadata__stac': Asset(os.path.join('.', 'test_file01.nc.stac.json'), title='metadata stac'),
+                                 })
+                with open(os.path.join(granules_dir, f'{filename}.nc.stac.json'), 'w') as ff:
+                    ff.write(json.dumps(stac_item.to_dict(False, False)))
+                catalog.add_link(Link('item', os.path.join('some_granules', f'{filename}.nc.stac.json'), 'application/json'))
             print(json.dumps(catalog.to_dict(False, False)))
             with open(os.environ['CATALOG_FILE'], 'w') as ff:
                 ff.write(json.dumps(catalog.to_dict(False, False)))
@@ -1828,13 +1832,12 @@ class TestDockerEntry(TestCase):
             upload_result = json.loads(upload_result_str)
             print(upload_result)
             self.assertTrue('features' in upload_result, 'missing features')
-            self.assertEqual(1, len(upload_result['features']), 'wrong length of upload_result features')
+            self.assertEqual(total_files, len(upload_result['features']), 'wrong length of upload_result features')
             upload_result = upload_result['features'][0]
             self.assertTrue('assets' in upload_result, 'missing assets')
             self.assertTrue('metadata__cas' in upload_result['assets'], 'missing assets#metadata__cas')
             self.assertTrue('href' in upload_result['assets']['metadata__cas'], 'missing assets#metadata__cas#href')
-            self.assertTrue(
-                upload_result['assets']['metadata__cas']['href'].startswith(f's3://{os.environ["STAGING_BUCKET"]}/{os.environ["COLLECTION_ID"]}/'))
+            self.assertTrue(upload_result['assets']['metadata__cas']['href'].startswith(f's3://{os.environ["STAGING_BUCKET"]}/{os.environ["COLLECTION_ID"]}/'))
             self.assertTrue('data' in upload_result['assets'], 'missing assets#data')
             self.assertTrue('href' in upload_result['assets']['data'], 'missing assets#data#href')
             self.assertTrue(upload_result['assets']['data']['href'].startswith(f's3://{os.environ["STAGING_BUCKET"]}/{os.environ["COLLECTION_ID"]}/'))
