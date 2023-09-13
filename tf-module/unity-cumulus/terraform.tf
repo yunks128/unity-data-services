d81ff4d89f6686900abb850b7899f6dd4da760b4
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-09-13 13:28:07 -0700

    fix: delete accidental commits

--- tf-module/unity-cumulus/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    region         = "us-west-2"
-    bucket         = "am-uds-dev-cumulus-tf-state"
-    key            = "am-uds-dev-cumulus/cumulus/unity/terraform.tfstate"
-    dynamodb_table = "am-uds-dev-cumulus-tf-locks"
-  }
-}
