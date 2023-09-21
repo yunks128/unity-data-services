fe0aebdf4602ae055edec32d89625398e04ae16d
wphyojpl <38299756+wphyojpl@users.noreply.github.com>
2023-09-21 13:20:28 -0700

    fix: cannot use >> in yaml (#213)
    * fix: add debug statement
    
    * fix: still finding out how to pass env
    
    * fix: cannot use >> in yaml
    
    * fix: add store version shell script in yml

--- ci.cd/store_version.sh
+++ ci.cd/store_version.sh
@@ -2,3 +2,5 @@
 software_version=`python3 ${GITHUB_WORKSPACE}/setup.py --version`
 echo $software_version
 echo "software_version=${software_version}" >> ${GITHUB_ENV}
+echo "PR_NUMBER1=${github.event.number}" >> ${GITHUB_ENV}
+echo "PR_TITLE1=${github.event.pull_request.title}" >> ${GITHUB_ENV}
