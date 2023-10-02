b8d9c2bd2fb99a87ff3ef1919925e939b1c2db3f
wphyojpl <38299756+wphyojpl@users.noreply.github.com>
2023-10-02 14:59:00 -0700

    fix: GitHub action write protected branch 5 (#231)
    * chore: dummy commit
    
    * fix: protected-branch error
    
    * fix: wrong version
    
    * fix: using pat token + shell script
    
    * fix: checking ENV
    
    * fix: revert some changes

--- ci.cd/update_version_commit.sh
+++ ci.cd/update_version_commit.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-env
 git status
 git diff
 current_branch=`git branch --show-current`
 git add -u
 git config --local user.email "wai.phyo@jpl.nasa.gov"
-git config --local user.name "wphyojpl"
+git config --local user.name ${GITHUB_TRIGGERING_ACTOR}
 git commit -m 'chore: update version + change log'
-git push origin $current_branch
+git push --force origin $current_branch
