133bdfd24ffe07a8c3cd35a198ce7d5567d144aa
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-10-02 15:47:59 -0700

    feat: create PR

--- ci.cd/update_version_commit.sh
+++ ci.cd/update_version_commit.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 git status
 git diff
-current_branch=`git branch --show-current`
-git add -u
 git config --local user.email "wai.phyo@jpl.nasa.gov"
 git config --local user.name ${GITHUB_TRIGGERING_ACTOR}
-echo ${GITHUB_TRIGGERING_ACTOR}
-git commit -m 'chore: update version + change log'
-git push --force origin $current_branch
+
+current_branch=`git branch --show-current`
+temp_branch="chore-version-update"
+commit_message="chore: update version + change log"
+git checkout -b
+git add -u
+git commit -m ${commit_message}
+git push --force origin $temp_branch
+pr_number=`gh pr create --base ${current_branch} --head ${$temp_branch} --title ${commit_message} | grep -oP '#\K\d+'`
+echo ${pr_number}
+gh pr review $pr_number --approve
+gh pr merge $pr_number --squash --merge
+git branch -D ${temp_branch}
+
