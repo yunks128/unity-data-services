9ec12ed3bc3bdbe2ddaae2ae6e5c729ec521d34b
wphyojpl <38299756+wphyojpl@users.noreply.github.com>
2023-10-02 16:46:52 -0700

    chore: update version + change log (#239)
    * chore: update version + change log
    
    * chore: update version + change log
    
    * chore: update version + change log
    
    * chore: update version + change log
    
    * chore: update version + change log

--- ci.cd/update_version_commit.sh
+++ ci.cd/update_version_commit.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
-git status
-git diff
-git config --local user.email "wai.phyo@jpl.nasa.gov"
-git config --local user.name ${GITHUB_TRIGGERING_ACTOR}
-
-current_branch=`git branch --show-current`
+#current_branch=`git branch --show-current`
+current_branch='develop-2023-10-02'
 temp_branch="chore-version-update"
 commit_message="chore: update version + change log"
-git checkout -b
+
+git stash
+git checkout -b ${temp_branch}
+git stash pop
+git status
+git config --local user.email "wai.phyo@jpl.nasa.gov"
+git config --local user.name ${GITHUB_TRIGGERING_ACTOR}
 git add -u
-git commit -m ${commit_message}
+git commit -m "${commit_message}"
 git push --force origin $temp_branch
-pr_number=`gh pr create --base ${current_branch} --head ${$temp_branch} --title ${commit_message} | grep -oP '#\K\d+'`
+result=`gh pr create --base "${current_branch}" --body "NA" --head "${temp_branch}" --title "${commit_message}"`
+echo $result
+pr_number=`echo $result | grep -oE '[0-9]+$'`
 echo ${pr_number}
-gh pr review $pr_number --approve
-gh pr merge $pr_number --squash --merge
+#gh pr review $pr_number --approve
+# hi
+gh pr merge $pr_number --squash --admin
 git branch -D ${temp_branch}
 
