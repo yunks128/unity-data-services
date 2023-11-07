350e78ff89ed990bbc9002c74372c6bd05d4a4e0
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-11-07 11:30:58 -0800

    fix: testing pagination issue

--- cumulus_lambda_functions/uds_api/web_service.py
+++ cumulus_lambda_functions/uds_api/web_service.py
@@ -27,7 +27,7 @@ async def root(request: Request):
 
 
 # to make it work with Amazon Lambda, we create a handler object
-handler = Mangum(app=app)
+handler = Mangum(app=app, api_gateway_base_path='/')  # TODO can ass stage from api_gateway_base_path, I think.
 
 if __name__ == '__main__':
     uvicorn.run("web_service:app", port=8005, log_level="info", reload=True)
