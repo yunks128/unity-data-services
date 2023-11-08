e70207653327c74f0a61644a5cacdeb8362f40af
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-11-08 09:42:11 -0800

    fix: updating base url env

--- cumulus_lambda_functions/uds_api/web_service_constants.py
+++ cumulus_lambda_functions/uds_api/web_service_constants.py
@@ -2,6 +2,6 @@ class WebServiceConstants:
     API_PREFIX = 'am-uds-dapa'
     COLLECTIONS = 'collections'
     DEPLOYED_STAGE = 'deployed_stage'
-    BASE_URL = 'base_url'
+    BASE_URL = 'UDS_BASE_URL'
     ITEMS = 'items'
     ADMIN = 'admin'
