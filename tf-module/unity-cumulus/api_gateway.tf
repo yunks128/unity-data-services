224c936537ccc7e06304eef58353edd6d3523fbf
Wai Phyo <wai.phyo@jpl.nasa.gov>
2024-01-02 17:45:53 -0600

    fix: add open api endpoint without authorization

--- tf-module/unity-cumulus/api_gateway.tf
+++ tf-module/unity-cumulus/api_gateway.tf
@@ -24,6 +24,7 @@ resource "aws_api_gateway_resource" "uds_api_base_resource" {
   path_part   = var.dapa_api_prefix
 }
 
+
 #
 # Creates the wildcard path (proxy+) resource, under the project resource
 #
@@ -38,8 +39,8 @@ resource "aws_api_gateway_method" "collection_method" {
   rest_api_id   = data.aws_api_gateway_rest_api.rest_api.id
   resource_id   = aws_api_gateway_resource.collection_resource.id
   http_method   = "ANY"
-  authorization = "NONE"
-#  authorizer_id = data.aws_api_gateway_authorizer.unity_cognito_authorizer.id
+  authorization = "CUSTOM"
+  authorizer_id = data.aws_api_gateway_authorizer.unity_cognito_authorizer.id
   request_parameters = {
     "method.request.path.proxy" = true
   }
@@ -64,7 +65,7 @@ resource "aws_api_gateway_integration" "collection_lambda_integration" {
 
 
 resource "aws_lambda_permission" "collection_lambda_integration__apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
+  statement_id  = "AllowExecutionFromAPIGatewayWildCard"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.uds_api_1.function_name
   principal     = "apigateway.amazonaws.com"
@@ -72,8 +73,50 @@ resource "aws_lambda_permission" "collection_lambda_integration__apigw_lambda" {
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:${var.aws_region}:${local.account_id}:${data.aws_api_gateway_rest_api.rest_api.id}/*/*${aws_api_gateway_resource.collection_resource.path}"
 }
+##########################################################################################################################
+resource "aws_api_gateway_resource" "openapi_resource" {
+  rest_api_id = data.aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_resource.uds_api_base_resource.id
+  path_part   = "openapi"
+}
+
+resource "aws_api_gateway_method" "openapi_method" {
+  rest_api_id   = data.aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.openapi_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "openapi_lambda_integration" {
+  rest_api_id   = data.aws_api_gateway_rest_api.rest_api.id
+  resource_id          = aws_api_gateway_resource.openapi_resource.id
+  http_method          = aws_api_gateway_method.openapi_method.http_method
+  type                 = "AWS_PROXY"
+  uri = aws_lambda_function.uds_api_1.invoke_arn
+  integration_http_method = "POST"
+
+#  cache_key_parameters = ["method.request.path.proxy"]
+
+  timeout_milliseconds = 29000
+#  request_parameters = {
+#    "integration.request.path.proxy" = "method.request.path.proxy"
+#  }
+}
 
 
+resource "aws_lambda_permission" "openapi_lambda_integration__apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGatewayOpenAPI"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.uds_api_1.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.aws_region}:${local.account_id}:${data.aws_api_gateway_rest_api.rest_api.id}/*/*${aws_api_gateway_resource.openapi_resource.path}"
+}
+##########################################################################################################################
 # The Shared Services API Gateway deployment
 resource "aws_api_gateway_deployment" "shared_services_api_gateway_deployment" {
   rest_api_id = data.aws_api_gateway_rest_api.rest_api.id
@@ -84,5 +127,5 @@ resource "aws_api_gateway_deployment" "shared_services_api_gateway_deployment" {
     create_before_destroy = true
   }
 
-  depends_on = [ aws_api_gateway_integration.collection_lambda_integration ]
+  depends_on = [ aws_api_gateway_integration.openapi_lambda_integration, aws_api_gateway_integration.collection_lambda_integration ]
 }
