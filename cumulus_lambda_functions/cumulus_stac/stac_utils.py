cca3dd9bcbc0f0c6da80d801b8a541fbeecdd75d
Wai Phyo <wai.phyo@jpl.nasa.gov>
2024-01-22 07:27:51 -0600

    feat: updating logic with new assets logic

--- cumulus_lambda_functions/cumulus_stac/stac_utils.py
+++ cumulus_lambda_functions/cumulus_stac/stac_utils.py
@@ -10,5 +10,10 @@ class StacUtils:
         """
         pystac_items = [ItemTransformer().from_stac(k) for k in input_stac_granules_list]
         assets = [k.get_assets() for k in pystac_items]
-        data_assets = [{'assets': {'data': k['data'].to_dict()}} for k in assets if 'data' in k]
-        return data_assets
+        data_only_assets = []
+        for each_granule_asset in assets:
+            data_only_asset = {k: v.to_dict() for k, v in each_granule_asset.items() if v.roles is not None and 'data' in v.roles}
+            data_only_assets.append(data_only_asset)
+        # assets = [k for k in assets if 'roles' in k and 'data' in k['role']]
+        # data_assets = [{'assets': {'data': k['data'].to_dict()}} for k in assets if 'data' in k]
+        return data_only_assets
