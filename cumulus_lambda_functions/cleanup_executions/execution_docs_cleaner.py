e21e1b47f24bd830d45500106715d4b9b990e506
Wai Phyo <wai.phyo@jpl.nasa.gov>
2024-02-16 15:00:22 -0600

    feat: add simple test case

--- cumulus_lambda_functions/cleanup_executions/execution_docs_cleaner.py
+++ cumulus_lambda_functions/cleanup_executions/execution_docs_cleaner.py
@@ -1,8 +1,11 @@
 import os
 
+from cumulus_lambda_functions.lib.lambda_logger_generator import LambdaLoggerGenerator
+
 from cumulus_lambda_functions.lib.time_utils import TimeUtils
 
 from cumulus_lambda_functions.cleanup_executions.cumulus_db_index import CumulusDbIndex
+LOGGER = LambdaLoggerGenerator.get_logger(__name__, LambdaLoggerGenerator.get_level_from_env())
 
 
 class ExecutionDocsCleaner:
@@ -13,5 +16,6 @@ class ExecutionDocsCleaner:
         cumulus_db = CumulusDbIndex()
         relative_time = int(os.environ.get('CUT_OFF_DAYS', 14))
         cut_off_time = TimeUtils().get_current_unix_milli() - TimeUtils.DAY_IN_MILLISECOND * relative_time
-        cumulus_db.delete_executions(cut_off_time)
+        result = cumulus_db.delete_executions(cut_off_time)
+        LOGGER.debug(f'deletion result for {cut_off_time}: {result}')
         return
