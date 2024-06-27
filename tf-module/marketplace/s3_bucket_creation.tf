fa36bd3189378fc883fcf1137767a214e0a2b372
wphyojpl <38299756+wphyojpl@users.noreply.github.com>
2024-06-27 14:12:44 -0700

    feat: market place bucket (#372)
    * feat: market place bucket
    
    * feat: create a new tf module for marketplace
    
    * feat: add marketplace terraform zip in github
    
    * feat: add example terraform files
    
    * feat: get new bucket to work with lambdas
    
    * fix: cumulus prefix is different from bucket name prefix
    
    * fix: updates for cross account
    
    * fix: remove root access to s3
    
    * feat: add prefix in ssm + read it from marketplace
    
    * fix: updating names + getting region from param
    
    * chore: some touchups
    
    * feat: use project + venue as requested by UCS
    
    * chore: add UCS marketplace requried variables

---
+++ tf-module/marketplace/s3_bucket_creation.tf
@@ -0,0 +1,50 @@
+data "aws_ssm_parameter" "uds_aws_account" {
+  name = var.uds_aws_account_ssm_path
+}
+
+data "aws_ssm_parameter" "uds_aws_account_region" {
+  name = var.uds_aws_account_region_ssm_path
+}
+
+data "aws_ssm_parameter" "uds_prefix" {
+  name = "arn:aws:ssm:${data.aws_ssm_parameter.uds_aws_account_region.value}:${data.aws_ssm_parameter.uds_aws_account.value}:parameter${var.uds_prefix_ssm_path}"
+}
+resource "aws_s3_bucket" "market_bucket" {
+  bucket = lower(replace("${var.project}-${var.venue}-unity-${var.market_bucket_name}", "_", "-"))
+  tags = merge(
+    var.tags,
+    {
+      "project" = var.project
+      "venue"   = var.venue
+    }
+  )
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "market_bucket" {  // https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_server_side_encryption_configuration
+  bucket = aws_s3_bucket.market_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "market_bucket" {
+  // https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_policy
+  bucket = aws_s3_bucket.market_bucket.id
+  policy = templatefile("${path.module}/s3_bucket_policy.json", {
+    udsAwsAccount: data.aws_ssm_parameter.uds_aws_account.value,
+    s3BucketName: aws_s3_bucket.market_bucket.id,
+    cumulus_lambda_processing_role_name: "${data.aws_ssm_parameter.uds_prefix.value}-${var.cumulus_lambda_processing_role_name_postfix}"
+  })
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.market_bucket.id
+  topic {
+    topic_arn     = "arn:aws:sns:${data.aws_ssm_parameter.uds_aws_account_region.value}:${data.aws_ssm_parameter.uds_aws_account.value}:${data.aws_ssm_parameter.uds_prefix.value}-granules_cnm_ingester"
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".json"
+    filter_prefix = var.market_bucket__notification_prefix
+  }
+}
