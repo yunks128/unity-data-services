704d3361b97a4eb3d5c175502d2acd5e76f22276
Wai Phyo <wai.phyo@jpl.nasa.gov>
2024-07-23 14:16:17 -0500

    feat: auto request part

--- tf-module/unity-cumulus/daac_archiver.tf
+++ tf-module/unity-cumulus/daac_archiver.tf
@@ -1,9 +1,41 @@
+resource "aws_lambda_function" "daac_archiver_request" {
+  filename      = local.lambda_file_name
+  source_code_hash = filebase64sha256(local.lambda_file_name)
+  function_name = "${var.prefix}-daac_archiver_request"
+  role          = var.lambda_processing_role_arn
+  handler       = "cumulus_lambda_functions.daac_archiver.lambda_function.lambda_handler_request"
+  runtime       = "python3.9"
+  timeout       = 300
+  environment {
+    variables = {
+      LOG_LEVEL = var.log_level
+      ES_URL = aws_elasticsearch_domain.uds-es.endpoint
+      ES_PORT = 443
+    }
+  }
+
+  vpc_config {
+    subnet_ids         = var.cumulus_lambda_subnet_ids
+    security_group_ids = local.security_group_ids_set ? var.security_group_ids : [aws_security_group.unity_cumulus_lambda_sg[0].id]
+  }
+  tags = var.tags
+}
+
+resource "aws_lambda_event_source_mapping" "granules_cnm_response_writer_lambda_trigger" {  // https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_event_source_mapping#sqs
+  event_source_arn = aws_sqs_queue.granules_cnm_response_writer.arn
+  function_name    = aws_lambda_function.daac_archiver_request.arn
+  batch_size = 1
+  enabled = true
+}
+
+#########################################
+
 resource "aws_lambda_function" "daac_archiver_response" {
   filename      = local.lambda_file_name
   source_code_hash = filebase64sha256(local.lambda_file_name)
   function_name = "${var.prefix}-daac_archiver_response"
   role          = var.lambda_processing_role_arn
-  handler       = "cumulus_lambda_functions.daac_archiver_response.lambda_function.lambda_handler_response"
+  handler       = "cumulus_lambda_functions.daac_archiver.lambda_function.lambda_handler_response"
   runtime       = "python3.9"
   timeout       = 300
   environment {
